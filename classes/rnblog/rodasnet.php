4da14e41ff03134f45e130896020ac4314a94262
Daniel Rodas <daniel.rodas1@hotmail.com>
2016-10-05 20:23:34 -0700

    Troublshooting Git

--- classes/rnblog/rodasnet.php
+++ classes/rnblog/rodasnet.php
@@ -8,15 +8,15 @@ use Pagination;
 use Rnblog\Model\Category;
 use Rnblog\Model\Post;
 
-class Rnblog_Rodasnet extends Rnblog_Driver
+class Rnblog_Rodasnet  extends Rnblog_Driver
 {
     /**
      * Driver specific functions
      *
      * /
-     *
-     *
-     * /**
+
+
+    /**
      * @param Pagination $pagination
      * @return array
      * @internal param $offset
@@ -32,10 +32,7 @@ class Rnblog_Rodasnet extends Rnblog_Driver
             ->get();
     }
 
-    /**
-     * @return int
-     */
-    public function ArticleCount()
+    public function showArticleCount()
     {
         return Post::count();
     }
@@ -45,7 +42,7 @@ class Rnblog_Rodasnet extends Rnblog_Driver
      * @param Model $article
      * @return Category | null
      */
-    public function getArticleSection(Model $article)
+    public function getArticleSection( Model $article)
     {
         return Category::query()->select('name')->where('id', $article->category_id)->get_one();
     }
@@ -55,108 +52,81 @@ class Rnblog_Rodasnet extends Rnblog_Driver
      * @param string $slug
      * @return \Orm\Model
      */
-    public function articleBySlug(string $slug)
+    public function articleBySlug( string $slug )
     {
         return Post::query()->where('slug', $slug)->related('author')->get_one();
     }
 
-    public function showArticlesRelated(Model $article)
+    public function showArticlesRelated( Model $article )
     {
         $category = Category::query()->where('id', $article->category_id)->get_one();
 
-        if (!$category) {
+        if ( ! $category)
+        {
             return null;
-        } else {
+        }
+        else
+        {
             return Post::query()->where('slug', '!=', $article->slug)
                 ->rows_limit(3)
                 ->related('category')
-                ->where('category.parent_id', $category->parent_id)
-                ->or_where('category.id', $category->parent_id)->get();
+                ->where('category.parent_id',  $category->parent_id)
+                ->or_where('category.id',  $category->parent_id)->get();
         }
     }
 
-    public function showCategoriesRelated(Model $article)
+    public function showCategoriesRelated( Model $article )
     {
         $category = Category::query()->where('id', $article->category_id)->get_one();
 
-        if (!$category) {
+        if ( ! $category)
+        {
             return false;
-        } else {
+        }
+        else
+        {
             return Category::query()
-                ->where('parent_id', $category->parent_id)
-                ->or_where('id', $category->parent_id)->get();
+                ->where('parent_id',  $category->parent_id)
+                ->or_where('id',  $category->parent_id)->get();
         }
     }
 
-    public function showMoreNews(Model $article)
+    public function showMoreNews( Model $article )
     {
-        $postRandom = rand(0, Post::count());
+        // Pagination
+        $config = array(
+            'pagination_url' => \Uri::current(),
+            'total_items'    => Post::count(),
+            'per_page'       => 3,
+            'uri_segment'    => 'page',
+        );
+        $pagination = Pagination::forge('more_news_pagination', $config);
+        // Get posts
         return Post::query()
-            ->offset(Post::count() - $postRandom)
-            ->limit(5)
-            ->where('category_id', '!=', $article->category_id)
+            ->offset($pagination->offset)
+            ->limit($pagination->per_page)
             ->order_by('created_at', 'DESC')
             ->get();
     }
 
     public function showFeaturedArticle()
     {
-        // TODO Find post that matches month and day
-        // SELECT * FROM `users` WHERE `email` LIKE "%@example.com" LIMIT 5 OFFSET 10
-//        $users = Model_User::find_by('email', '%@example.com', 'like', 5, 10);
-
-        // Or.. http://www.richardlord.net/blog/dates-in-php-and-mysql
-
-//        $query = "UPDATE table SET
-//    datetimefield = FROM_UNIXTIME($phpdate)
-//    WHERE...";
-//        $query = "SELECT UNIX_TIMESTAMP(datetimefield)
-//    FROM table WHERE...";
-
-        // Thu Sep 29 18:55:57 2016
-//        $timestamp = \Date::forge();
-
-//        $post = Post::find_by('created_at', '%@example.com', 'like', 5, 10);
-
-//        ->where('created_at', $FeaturedId )->get_one()
-
-        if (!$post = null)
-        {
-            $FeaturedId = $this->computeFeaturedId(date('z'));
-
-            if (!$post = Post::query()->where('id', $FeaturedId)->get_one())
-            {
-                return Post::query()->where('id', 25)->get_one();
-            }
-        }
-
-        return $post;
+        return Post::query()->where('id', 25)->get_one();
     }
 
-    public function showFeaturedImageEncodedBase64(Model $article)
+    public function showFeaturedImageEncodedBase64( Model $article )
     {
-        if ($post = Post::query()->where('slug', $article->slug)->get_one())
+        if ( $post = Post::query()->where('slug', $article->slug)->get_one() )
         {
-            if ($gallery = Model_Gallery::query()->where('post_id', $post->id)->get_one())
-            {
-                $data['url'] = $gallery->asset->uri . '' . $gallery->asset->name;
-                $data['extension'] = $gallery->asset->type;
-                return \Request::forge('image/encoder/encodeBase64')
+            $gallery = Model_Gallery::query()->where('post_id', $post->id)->get_one();
+            $data['url'] = $gallery->asset->uri . '' . $gallery->asset->name;
+            $data['extension'] = $gallery->asset->type;
+            return \Request::forge('image/encoder/encodeBase64')
                     ->execute($data)->response()->body();
-            }
         }
 
         return false;
     }
 
-    protected function computeFeaturedId($FeaturedId)
-    {
-        if (Post::count() < $FeaturedId)
-        {
-            return $FeaturedId = abs(round($FeaturedId / 2));
-        }
-
-        return $FeaturedId;
-    }
 
 }
